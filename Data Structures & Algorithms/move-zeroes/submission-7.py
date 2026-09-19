6ce9d73145ccd8f3ae5734fd3cb69b03ce3e3c43
Aditi Anand <aditianandkumar@gmail.com>
2026-09-19 09:58:15 +0530

    Add: move-zeroes - submission-7

---
+++ Data Structures & Algorithms/move-zeroes/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        #better solution is to keep two pointers 
+        i=0 #to keep track of 0 elements 
+        for num in range(0,n,1):
+            if(nums[num]!=0):
+                nums[i],nums[num]=nums[num],nums[i] #swap 
+                i+=1 #move the pointer
+                    
